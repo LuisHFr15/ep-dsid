c82a22e35385093fb79b014d41929230f6bc6c83
Luis H F Ramires <luishenriquefr@outlook.com>
2026-07-07 14:34:39 -0300

    refactor(hub): retire in-memory prototype superseded by dynamo

--- hub/src/application/files/list-files.ts
+++
@@ -1,9 +0,0 @@
-import { MemoryStore } from "../../infrastructure/memory/memory-store";
-
-export class ListFiles {
-  constructor(private readonly store: MemoryStore) {}
-
-  execute() {
-    return Array.from(this.store.files.values());
-  }
-}
