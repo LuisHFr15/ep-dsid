817faba7118ebcf0b26e7fd71d39bdaad612b3dc
Luis H F Ramires <luishenriquefr@outlook.com>
2026-07-18 17:12:14 -0300

    chore: remove electron-ui superseded by unified client

--- electron-ui/src/preload/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getHubUrl: (): Promise<string> => ipcRenderer.invoke('app:getHubUrl'),
-  openFilePicker: (): Promise<string | null> => ipcRenderer.invoke('dialog:openFile'),
-  torrentStatus: () => ipcRenderer.invoke('torrent:status')
-})
